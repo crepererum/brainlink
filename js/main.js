502ead6dc7a25e3a122c0fe19a7c1834c5f7414c
Marco Neumann <marco@crepererum.net>
2013-07-28 15:11:09 +0200

    Implement basic render routine

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
 var canvas, ctx;
 
+var players = [];
+
+function Finger() {
+	"use strict";
+	var self = this;
+
+	self.id = 0;
+	self.x = 0;
+	self.y = 0;
+}
+
+function Player() {
+	"use strict";
+	var self = this;
+
+	self.id = 0;
+	self.x = 0;
+	self.y = 0;
+	self.fingers = [];
+}
+
+function render() {
+	"use strict";
+	var i, j, finger, player;
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+
+	for (i = 0; i < players.length; ++i) {
+		player = players[i];
+
+		ctx.fillStyle = "#ffffff";
+
+		ctx.font = "30px Arial";
+		ctx.fillText(player.id, player.x, player.y);
+
+		for (j = 0; j < player.fingers.length; ++j) {
+			finger = player.fingers[j];
+
+			ctx.font = "15px Arial";
+			ctx.fillText(finger.id, finger.x, finger.y);
+		}
+	}
+
+	window.requestAnimationFrame(render);
+}
+
 function init() {
 	"use strict";
 
@@ -8,6 +57,8 @@ function init() {
 	canvas.width = canvas.clientWidth;
 
 	ctx = canvas.getContext("2d");
+
+	window.requestAnimationFrame(render);
 }
 
 window.onload = init;
